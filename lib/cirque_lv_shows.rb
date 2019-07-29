356d0d0abb6c182acc5b936ab78a36754a548a2a
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-29 05:07:28 -0700

    added new libraries in environment

--- lib/cirque_lv_shows.rb
+++ lib/cirque_lv_shows.rb
@@ -1,8 +1,10 @@
-#acting as an environment
+#environment
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
 require_relative "cirque_lv_shows/version"
-require_relative './cirque_lv_shows/shows'
+require_relative './cirque_lv_shows/scraper'
 require_relative './cirque_lv_shows/cli'
+require_relative './cirque_lv_shows/shows'
+require_relative './cirque_lv_shows/schedule'
